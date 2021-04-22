361cb4b4d1491b9e79ac30f170a91eed1895ccd2
Booked61 <81597212+Booked61@users.noreply.github.com>
2021-04-21 17:18:42 -0700

    Add files via upload

---
+++ Knockback (With config)/src/com/booked/knockback/Knockback.java
@@ -0,0 +1,40 @@
+package com.booked.knockback;
+
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Knockback extends JavaPlugin implements Listener {
+
+    @Override
+    public void onEnable() {
+        getServer().getPluginManager().registerEvents(this, this);
+        saveDefaultConfig();
+        getServer().getConsoleSender().sendMessage("§4Knockback plugin enabled! \uD83D\uDE03\n" +
+                "\n" +
+                "§dCheck the config.yml to change the knockback and for information on how to contact me!\n" +
+                "§bThis plugin was made by booked61 and can be used with his permission. See config.yml for contact information!");
+    }
+
+    @Override
+    public void onDisable() {
+    }
+
+
+    @EventHandler
+    public void onPlayerHit(EntityDamageByEntityEvent e){
+        if (e.getEntity() instanceof Player) {
+            Entity p = e.getEntity();
+            p.setVelocity(e.getDamager().getLocation().getDirection().multiply(getConfig().getInt("knockback-of-mobs")));
+        } else if (e.getEntity() != null) {
+            Entity p = e.getEntity();
+            p.setVelocity(e.getDamager().getLocation().getDirection().multiply(getConfig().getInt("knockback-of-mobs")));
+        }
+    }
+}
+
